5aaa619cc267c6aca58eee68fe4c3995f49c0029
Takkuya <takuyagabriel@gmail.com>
2023-01-30 14:09:42 -0300

    Confirmation Form

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,32 @@
 import { Calendar } from '@/src/components/Calendar'
-import { Container } from './styles'
+import { Container, TimePicker, TimePickerHeader, TimePickerList, TimePickerItem } from './styles'
 
 export const CalendarStep = () => {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>30 de janeiro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>
+              15:00h
+            </TimePickerItem>
+            <TimePickerItem>
+              15:00h
+            </TimePickerItem>
+            <TimePickerItem>
+              15:00h
+            </TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
